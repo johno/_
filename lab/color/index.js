4c5ee1a0728774e6ed06ebd1750f03d883d74368
John Otander <johnotander@gmail.com>
2018-11-17 13:26:29 -0700

    Add lab section

---
+++ lab/color/index.js
@@ -0,0 +1,9 @@
+const Jimp = require('jimp')
+
+module.exports = (size = 64, color = '#444') => {
+  const [width, height] = size.toString().split('x')
+  const hexColor = Jimp.cssColorToHex(color)
+  return new Jimp(width, height || width, hexColor, (err, image) => {
+    return image
+  })
+}

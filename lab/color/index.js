50e34f10ac283591c89f8a2db19a6cb5ca6a749c
John Otander <johnotander@gmail.com>
2018-11-27 09:40:27 -0700

    Spike out color lab

--- lab/color/index.js
+++ lab/color/index.js
@@ -1,9 +1,18 @@
+const { send } = require('micro')
 const Jimp = require('jimp')
+const getParam = require('get-query-param')
+const img = require('./img-color')
 
-module.exports = (size = 64, color = '#444') => {
-  const [width, height] = size.toString().split('x')
-  const hexColor = Jimp.cssColorToHex(color)
-  return new Jimp(width, height || width, hexColor, (err, image) => {
-    return image
+module.exports = (req, res) => {
+  const size = getParam('size', req.url) || 64
+  const color = getParam('color', req.url) || '#444'
+  res.setHeader('Content-Type', Jimp.MIME_JPEG)
+
+  img(size, color).getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+    if (err) {
+      send(res, 500, 'An error occurred')
+    } else {
+      send(res, 200, buffer)
+    }
   })
 }

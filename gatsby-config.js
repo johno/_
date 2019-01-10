6dd138d837efcbc54678fb7aab50ec3efae6e8bd
John Otander <johnotander@gmail.com>
2019-01-09 18:29:11 -0800

    Use gatsby-theme-sideways

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  siteMetadata: {
+    title: 'johno',
+    twitterHandle: '4lpine',
+    githubHandle: 'johno'
+  },
   __experimentalThemes: [
     {
-      resolve: 'gatsby-theme-minimal-blog',
+      resolve: 'gatsby-theme-sideways',
       options: {}
     }
   ]

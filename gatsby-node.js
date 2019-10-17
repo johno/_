a0b3ab5825a99c6de074d6f4f3492b71c0e97b35
John Otander <johnotander@gmail.com>
2019-10-17 09:40:29 -0600

    Continue working on jsx post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,7 @@
-const replacePath = path =>
-  path === '/writing/archives' ? path : path.replace(/^\/writing\/archives/, '')
-
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage, deletePage } = actions
-
-  const oldPage = { ...page }
-  page.path = replacePath(page.path)
-
-  if (page.path !== oldPage.path) {
-    deletePage(oldPage)
-    createPage(page)
-  }
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    node: {
+      fs: 'empty'
+    }
+  })
 }

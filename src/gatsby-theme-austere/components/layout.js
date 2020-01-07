c02c7d350b8baedd841585bead0417277dd66cae
John Otander <johnotander@gmail.com>
2020-01-07 12:01:10 -0700

    Add newsletter to theme layout

---
+++ src/gatsby-theme-austere/components/layout.js
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+import Layout from 'gatsby-theme-austere/src/components/layout'
+import Subscribe from '../../components/subscribe'
+
+export default ({ skipSubscribe, ...props }) => (
+  <div>
+    <title>John Otander</title>
+    <Layout {...props} />
+    {skipSubscribe ? null : (
+      <Styled.root>
+        <Styled.hr />
+        <div sx={{ maxWidth: 'container', mt: [4, 5, 6], mx: 'auto', px: 3 }}>
+          <Subscribe>Sign up for my newsletter</Subscribe>
+        </div>
+      </Styled.root>
+    )}
+  </div>
+)

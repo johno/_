e42900e4b42b25825ac1ae12abd6df0e5b85ba43
John Otander <johnotander@gmail.com>
2020-01-07 11:20:00 -0700

    Add silly arrow logo

--- src/gatsby-theme-austere/components/logo.js
+++ src/gatsby-theme-austere/components/logo.js
@@ -1,1 +1,16 @@
-export default () => null
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { Link } from 'gatsby'
+
+export default () => (
+  <Link
+    to="/"
+    sx={{
+      color: 'inherit',
+      textDecoration: 'none',
+      '&:hover': { color: 'tomato' }
+    }}
+  >
+    ↟
+  </Link>
+)

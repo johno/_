a1f803cd1fc31e5715b7cf7fcfa42abac29bafd9
John Otander <johnotander@gmail.com>
2020-07-24 10:08:43 -0600

    Update gatsby-blog-theme-tags

--- packages/gatsby-theme-blog-tags/src/templates/tag-query.js
+++ packages/gatsby-theme-blog-tags/src/templates/tag-query.js
@@ -7,16 +7,14 @@ export default TagPage
 export const query = graphql`
   query TagPageQuery($tag: String!) {
     allBlogPost(filter: { tags: { in: [$tag] } }) {
-      edges {
-        node {
-          id
-          excerpt
-          body
-          slug
-          title
-          tags
-          date(formatString: "MMMM DD, YYYY")
-        }
+      nodes {
+        id
+        excerpt
+        body
+        slug
+        title
+        tags
+        date(formatString: "MMMM DD, YYYY")
       }
     }
   }

8832c508631007606b5ebc1173569cc16b8ab67d
John Otander <johnotander@gmail.com>
2020-11-16 09:59:43 -0700

    Add second lesson in vim

---
+++ content/notes/mdx-to-jsx.js
@@ -0,0 +1,14 @@
+---
+title: MDX to JS(X)
+slug: /mdx-to-jsx
+date: 2020-11-12
+tags:
+  - mdx
+---
+
+
+
+## Related
+
+- https://github.com/mdx-js/mdx/issues/1152
+- https://twitter.com/kuvos/status/1326864979920818177

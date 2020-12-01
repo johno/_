a214dc5c13a57abbba1bae056406e7422cf474b8
John Otander <johnotander@gmail.com>
2020-12-01 14:15:04 -0700

    Add some og stuffs

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -1,14 +1,23 @@
-import { h, Fragment } from 'preact'
-import { Helmet } from 'react-helmet'
+import { h, Fragment } from "preact";
+import { Helmet } from "react-helmet";
 
-export default ({ children, title, ...props }) => {
+export default ({
+  children,
+  title = "johno.com",
+  description = "Writing about code and design",
+  ...props
+}) => {
   return (
     <div>
       <Helmet>
         <link rel="stylesheet" href="/styles.css" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
       </Helmet>
       {title ? <h1>{title}</h1> : null}
       {children}
     </div>
-  )
-}
+  );
+};

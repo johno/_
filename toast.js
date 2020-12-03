f2bfe67b64570b01518423b3aa4fd9f3ac7de015
John Otander <johnotander@gmail.com>
2020-12-03 06:59:39 -0700

    Properly add showTitle to post data

--- toast.js
+++ toast.js
@@ -1,56 +1,56 @@
-import { fetchMdxFromDisk } from '@toastdotdev/mdx'
-import mdx from '@mdx-js/mdx'
-import fm from 'gray-matter'
-import sortBy from 'lodash.sortby'
+import { fetchMdxFromDisk } from "@toastdotdev/mdx";
+import mdx from "@mdx-js/mdx";
+import fm from "gray-matter";
+import sortBy from "lodash.sortby";
 
 const defaultPostSlug = (filename) =>
-  filename.slice('content/posts'.length, filename.length).replace(/.mdx$/, '')
+  filename.slice("content/posts".length, filename.length).replace(/.mdx$/, "");
 
 export const sourceData = async ({ setDataForSlug }) => {
-  const postData = []
+  const postData = [];
   const [srcPages, posts] = await Promise.all([
     fetchMdxFromDisk({
-      directory: './src/pages',
+      directory: "./src/pages",
     }),
     fetchMdxFromDisk({
-      directory: './src/posts',
+      directory: "./src/posts",
     }),
-  ])
+  ]);
 
   const allMdx = srcPages
     .map(({ filename, file }) => {
       const slug = filename
-        .slice('src/pages'.length, filename.length)
-        .replace(/.mdx?$/, '')
-      return { slug, filename, file }
+        .slice("src/pages".length, filename.length)
+        .replace(/.mdx?$/, "");
+      return { slug, filename, file };
     })
     .concat(
       posts.map(({ filename, file }) => {
-        const { data, content } = fm(file)
-        const slug = data.slug || defaultPostSlug(filename)
+        const { data, content } = fm(file);
+        const slug = data.slug || defaultPostSlug(filename);
 
-        postData.push({ ...data, slug })
+        postData.push({ ...data, slug, showTitle: true });
 
-        return { slug, filename, file: content, data, showTitle: true }
+        return { slug, filename, file: content, data };
       })
-    )
+    );
 
   Promise.all(
     allMdx.map(async ({ file, slug, data }) => {
-      let compiledMdx = await mdx(file)
+      let compiledMdx = await mdx(file);
       const pageData =
-        slug === '/writing/archives'
-          ? { ...data, posts: sortBy(postData, 'date').reverse() }
-          : data
+        slug === "/writing/archives"
+          ? { ...data, posts: sortBy(postData, "date").reverse() }
+          : data;
       await setDataForSlug(slug, {
         component: {
-          mode: 'source',
+          mode: "source",
           value: `/** @jsx mdx */
 import { mdx } from '@mdx-js/preact';
 ${compiledMdx}`,
         },
         data: pageData,
-      })
+      });
     })
-  )
-}
+  );
+};
